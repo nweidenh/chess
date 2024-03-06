fee684ea2da841cb0fad2c7f83cc2e06ebf91a28
Nathan W <weidenhamer.nathan@gmail.com>
2024-03-05 20:42:00 -0700

    Mapped out directory for data access tests

---
+++ server/src/test/java/dataAccessTests/dataAccessTestsSQL.java
@@ -0,0 +1,18 @@
+package dataAccessTests;
+
+import chess.ChessGame;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import service.*;
+import dataAccess.*;
+import model.*;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class dataAccessTestsSQL {
+}
