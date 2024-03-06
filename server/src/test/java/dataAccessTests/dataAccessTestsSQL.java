ba8fd5bb29fd43b1c2d2af119107c584311c88fe
Nathan W <weidenhamer.nathan@gmail.com>
2024-03-05 23:32:54 -0700

    Working on tests

--- server/src/test/java/dataAccessTests/dataAccessTestsSQL.java
+++ server/src/test/java/dataAccessTests/dataAccessTestsSQL.java
@@ -15,4 +15,42 @@ import java.util.Collection;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class dataAccessTestsSQL {
+
+    private final UserService userService;
+    private final AuthService authService;
+    private final GameService gameService;
+
+    public dataAccessTestsSQL(){
+        UserDAO usersDataAccess = null;
+        try {
+            usersDataAccess = new SQLUserDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        AuthDAO authsDataAccess = null;
+        try {
+            authsDataAccess = new SQLAuthDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        GameDAO gamesDataAccess = null;
+        try {
+            gamesDataAccess = new SQLGameDAO();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        userService = new UserService(usersDataAccess, authsDataAccess, gamesDataAccess);
+        authService = new AuthService(usersDataAccess, authsDataAccess, gamesDataAccess);
+        gameService = new GameService(usersDataAccess, authsDataAccess, gamesDataAccess);
+    }
+
+    @BeforeEach
+    public
+
+    @Test
+    @DisplayName("Successful Create")
+    void create() throws DataAccessException{
+        var user = new UserData("newUser", "strongPassword", "advancedEmail@gmail.com");
+
+    }
 }
